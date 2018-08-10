c9ec745e1b12644bf1b7a21c093e34ee987b2ec9
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-10 19:35:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,18 @@
 
 def fizzbuzz(num)
   if num % 3 == 0 
-    "Fizz"
+    print "Fizz"
   end
   if num % 5 == 0 
-    "Buzz" 
+    print "Buzz" 
   end
   
-(num % 3 == 0 && num % 5 == 0 ) ? puts("FizzBuzz") : puts(nil)
+(num % 3 == 0 && num % 5 == 0 ) ? print("FizzBuzz") : print(nil)
+
+end
+
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+  end
 end
