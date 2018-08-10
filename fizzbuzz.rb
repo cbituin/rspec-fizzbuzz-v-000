126104b4a25ba3ca346ea96bef9c174a88579394
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-10 19:36:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,20 +1,3 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(num)
-  if num % 3 == 0 
-    print "Fizz"
-  end
-  if num % 5 == 0 
-    print "Buzz" 
-  end
-  
-(num % 3 == 0 && num % 5 == 0 ) ? print("FizzBuzz") : print(nil)
-
-end
-
-# def fizzbuzz(int)
-#   if int % 3 == 0 # if the number int is divisible by 3
-#     "Fizz" # Go fizz
-#   end
-# end
