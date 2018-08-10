d06ecd19e010aee1ab4d64e8e32436ca714388fe
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-10 19:22:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,10 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0 
     puts "FizzBuzz"
-  end
-  if num % 3 == 0 
+  elsif num % 3 == 0 
     puts "Fizz"
   elsif num % 5 == 0 
     puts "Buzz" 
   else
     puts NIL
-  end
 end
