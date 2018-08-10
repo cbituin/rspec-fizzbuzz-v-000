ed60ef2d40081e7994daa3c8659f8d759c5aea50
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-10 19:36:02 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -13,8 +13,8 @@ def fizzbuzz(num)
 
 end
 
-def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
-  end
-end
+# def fizzbuzz(int)
+#   if int % 3 == 0 # if the number int is divisible by 3
+#     "Fizz" # Go fizz
+#   end
+# end
